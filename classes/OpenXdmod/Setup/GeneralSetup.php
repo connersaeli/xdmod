bbe546f0b831227f305c683a95604ba8be49cc83
Conner Saeli <connersa@buffalo.edu>
2026-04-10 11:22:50 -0400

    Refactoring dotenv setup and migration.

--- classes/OpenXdmod/Setup/GeneralSetup.php
+++ classes/OpenXdmod/Setup/GeneralSetup.php
@@ -133,22 +133,6 @@ EOT
             'app_secret' => hash('sha512', time())
         ]);
         $this->saveTemplate($envTemplate, BASE_DIR . '/.env');
-
-        // Make sure to clear the cache before dumping the dotenv so we start clean.
-        try {
-            SymfonyCommandHelper::executeCommand('cache:clear');
-        } catch (\Exception $e) {
-            throw new \RuntimeException('Error occurred executing cache:clear', $e);
-        }
-
-
-        // Dump dotenv data so we don't read .env each time in prod.
-        // Note: this means that if you want to start debugging stuff you'll need to delete the generated .env.
-        try {
-            SymfonyCommandHelper::executeCommand("dotenv:dump");
-        } catch (\Exception $e) {
-            throw new \RuntimeException('Error occurred executing dotenv:dump', $e);
-        }
-
+        SymfonyCommandHelper::dumpDotEnv();
     }
 }
--- src/Helper/SymfonyCommandHelper.php
+++ src/Helper/SymfonyCommandHelper.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Dotenv\Dotenv\Exception\FormatException;
+use Symfony\Component\Dotenv\Dotenv\Exception\PathException;
 
 /**
  * The purpose of this class is to serve as a bridge to allow our existing code in `classes` to execute Symfony Commands
@@ -17,66 +19,56 @@ use Symfony\Component\Dotenv\Dotenv;
  */
 class SymfonyCommandHelper
 {
-
     /**
-     * Execute the provided Symfony $command.
-     *
-     * @param string $command the Symfony command to run.
-     * @param array $options for the Symfony command.
-     * @throws \LogicException if the command is empty.
-     * @throws \Exception if an error is encountered while running the specified command.
-     * @throws \RuntimeException if a non-zero exit code is returned by the Symfony Command.
-     */
-    public static function executeCommand(string $command, string $env = 'prod', bool $debug = false, array $options = []): void
-    {
-        list($statusCode, $output) = self::doExecuteCommand($command, $env, $debug, $options);
-        if ($statusCode !== 0) {
-            throw new \RuntimeException("Error Running $command\n$output");
-        }
-    }
-
-    /**
-     * The function that actually executes the Symfony commmand.
+     * Execute the provided Symfony console command.
      *
      * @param string $command
+     * @param array $options
      * @param string $env
      * @param bool $debug
-     * @param array $options
-     * @return array
-     * @throws \LogicException
-     * @throws \Exception
-     * @throws \RuntimeException
+     * @throws \LogicException if the command is empty.
+     * @throws \RuntimeException if the environment file cannot be loaded or
+     *  if a non-zero exit code is returned by the Symfony console command
      */
-    private static function doExecuteCommand(string $command, string $env, bool $debug, array $options): array
+    private static function executeCommand(string $command, array $options = [], string $env = 'prod', bool $debug = false): void
     {
         if (empty($command)) {
             throw new \LogicException('Command must not be empty.');
         }
 
         try {
-            $envPath = BASE_DIR . "/.env";
+            $envPath = BASE_DIR . '/.env';
             (new Dotenv())->bootEnv($envPath);
-        } catch(\Exception $e) {
-            throw new \RuntimeException('Error booting the Symfony Environment', $e->getCode(), $e);
+        } catch(FormatException | PathException $e) {
+            throw new \RuntimeException('Unable to load environment file', $e->getCode(), $e);
         }
 
-        // Setup our Kernel / Application.
+        // Setup our Kernel / Application
         $kernel = new Kernel($env, $debug);
         $application = new Application($kernel);
 
         // we set this so that it doesn't `exit` whatever php script is calling this function.
         $application->setAutoExit(false);
 
-        // Set the Symfony command that is to be executed.
+        // Set the Symfony command to execute
         $options['command'] = $command;
 
         $input = new ArrayInput($options);
         $output = new BufferedOutput();
-        try {
-            $statusCode = $application->run($input, $output);
-            return [$statusCode, $output->fetch()];
-        } catch(\Exception $e) {
-            throw new \RuntimeException("Error while running Symfony Command", $e->getCode(), $e);
+        $statusCode = $application->run($input, $output);
+
+        if ($statusCode !== 0) {
+            throw new \RuntimeException('Error running ' . $command . '\n' . $output->fetch());
         }
+    }
+
+    public static function dumpDotEnv(): void
+    {
+        // Make sure to clear the cache before dumping the dotenv so we start clean.
+        SymfonyCommandHelper::executeCommand('cache:clear');
+
+        // Dump dotenv data so we don't read .env each time in prod.
+        // Note: this means that if you want to start debugging stuff you'll need to delete the generated .env.
+        SymfonyCommandHelper::executeCommand('dotenv:dump');
     }
 }
