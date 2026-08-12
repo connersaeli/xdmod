5cce6e31901032e400b204540b49b59625ec6186
Conner Saeli <connersa@buffalo.edu>
2026-08-12 11:53:42 -0400

    Trying different access denied mechanism.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -26,11 +26,12 @@ class AdminController extends BaseController
      * @throws Exception if the user calling this endpoint is not authorized to do so.
      * @throws BadRequestHttpException if no user is found for the provided uid.
      * @throws BadRequestHttpException if the viewedTour parameter is any integer value other than 0 or 1.
+     * #[IsGranted("mgr")]
      */
-    #[IsGranted("mgr")]
     #[Route('{prefix}reset_user_tour_viewed', requirements: ['prefix' => '.*'], methods: ['POST'])]
     public function resetUserTourViewed(Request $request): Response
     {
+        $this->denyAccessUnlessGranted('mgr');
         $viewedTour = $this->getIntParam($request, 'viewedTour', true);
         $selectedUser = XDUser::getUserByID(
             $this->getIntParam($request, 'uid', true)
